09102281c8e49316219bcedad6a4793c4580184d
Sergei Belonosov <2belonosov@gmail.com>
2018-07-12 12:28:25 +0300

    ADD: A test to loading question is created.

--- src/test/java/com/mdevi/examboot/ExamBootApplicationTests.java
+++ src/test/java/com/mdevi/examboot/ExamBootApplicationTests.java
@@ -1,16 +1,32 @@
 package com.mdevi.examboot;
 
+import com.mdevi.examboot.model.Question;
+import com.mdevi.examboot.service.TestQuestionsLoader;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ExamBootApplicationTests {
 
+	@Autowired
+	TestQuestionsLoader loader;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void shouldLoadFiveQuestionsOnly(){
+		List<Question> questionList = loader.loadTestQuestions();
+		assertEquals(5, questionList.size());
 	}
 
 }
